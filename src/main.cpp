8eff0be70432050c5cf2d81456ae58993afa62b9
Kogepan25629 <kogepan25629@gmail.com>
2023-09-05 17:51:10 +0800

    Set project name

--- src/main.cpp
+++ src/main.cpp
@@ -393,7 +393,7 @@ int main(int, char**) {
     // Create window with Vulkan context
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     GLFWwindow* window = glfwCreateWindow(
-        1280, 720, ("Dear ImGui GLFW+Vulkan example " + std::string(PROJECT_VERSION)).c_str(), nullptr, nullptr
+        1280, 720, ("H8 Practice Kit Simulator " + std::string(PROJECT_VERSION)).c_str(), nullptr, nullptr
     );
     if (!glfwVulkanSupported()) {
         printf("GLFW: Vulkan Not Supported\n");
