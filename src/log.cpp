d7aaef7197902437901af2bc710e4ec81faedff2
Kogepan25629 <kogepan25629@gmail.com>
2023-09-11 15:39:08 +0800

    Add debug log

--- src/log.cpp
+++ src/log.cpp
@@ -5,8 +5,19 @@
 
 namespace log {
 
-void info(std::string message) { std::cout << "[info] " + message << std::endl; }
-void warn(std::string message) { std::cout << "[warn] " + message << std::endl; }
-void error(std::string message) { std::cout << "[error] " + message << std::endl; }
+void info(std::string message) {
+    std::cout << "[info] " + message << std::endl;
+}
+void warn(std::string message) {
+    std::cout << "[warn] " + message << std::endl;
+}
+void error(std::string message) {
+    std::cout << "[error] " + message << std::endl;
+}
+void debug(std::string message) {
+#ifndef NDEBUG
+    std::cout << "[debug] " + message << std::endl;
+#endif
+}
 
 }  // namespace log
--- src/log.h
+++ src/log.h
@@ -7,5 +7,6 @@ namespace log {
 void info(std::string message);
 void warn(std::string message);
 void error(std::string message);
+void debug(std::string message);
 
 }  // namespace log
