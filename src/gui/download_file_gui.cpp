c4b9fa7004a93236ad68737692789251daad4c31
Kogepan25629 <kogepan25629@gmail.com>
2023-09-12 20:41:47 +0800

    Download file async

--- src/gui/download_file_gui.cpp
+++ src/gui/download_file_gui.cpp
@@ -2,6 +2,10 @@
 
 #include <download_file.h>
 
+#include <chrono>
+#include <future>
+#include <thread>
+
 #include "gui/async_gui.h"
 #include "gui/download_file_gui.h"
 #include "imgui.h"
@@ -11,7 +15,7 @@ namespace gui {
 DownloadFileGui::DownloadFileGui(std::string url, std::string desc_dir_path) {
     content_length  = 0;
     received_length = 0;
-    network::download_file(url, desc_dir_path, &content_length, &received_length);
+    result_ft       = std::async(network::download_file, url, desc_dir_path, &content_length, &received_length);
 }
 
 DownloadFileGui::~DownloadFileGui() {
@@ -19,6 +23,25 @@ DownloadFileGui::~DownloadFileGui() {
 
 void DownloadFileGui::update() {
     ImGui::Begin("Download file");
+
+    if (result_ft.valid()) {
+        using namespace std::chrono_literals;
+        auto status = result_ft.wait_for(0ms);
+        if (status == std::future_status::ready) {
+            finished = true;
+            result   = result_ft.get();
+        }
+    }
+
+    if (finished) {
+        if (result.error.empty()) {
+            ImGui::TextUnformatted("Download finished!");
+        } else {
+            ImGui::TextUnformatted(result.error.c_str());
+        }
+    } else {
+        ImGui::TextUnformatted("Downloading ...");
+    }
     ImGui::Text("%d/%d", received_length, content_length);
     ImGui::End();
 }
--- src/gui/download_file_gui.h
+++ src/gui/download_file_gui.h
@@ -1,12 +1,17 @@
 #pragma once
 
+#include <future>
 #include <string>
 
 #include "async_gui.h"
+#include "download_file.h"
 
 namespace gui {
 
 class DownloadFileGui : public gui::AsyncGui {
+    bool finished;
+    std::future<network::DownloadFileResult> result_ft;
+    network::DownloadFileResult result;
     void update() override;
 
    public:
