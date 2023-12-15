072c29bceb1e8587f2cc9540701f4510183124c6
Kogepan229 <kogepan25629@gmail.com>
2023-12-15 20:21:18 +0900

    Add get_dns for linux

--- src/network/dns.cpp
+++ src/network/dns.cpp
@@ -1,19 +1,30 @@
 #include "dns.hpp"
 
+#include <arpa/inet.h>
+#include <bits/types/res_state.h>
+#include <netinet/in.h>
+
+#include <format>
+
+#if defined(_WIN32) || defined(_WIN64)
 // Must include before windows.h
 #include <winsock2.h>
 // Next
 #include <iphlpapi.h>
 #include <stdio.h>
 #include <winerror.h>
+#else
+#include <cstdlib>
+
+#include "resolv.h"
+#endif
 
 #include <string>
-
-#include "log.h"
 
 namespace network {
 
 std::string get_dns() {
+#if defined(_WIN32) || defined(_WIN64)
     FIXED_INFO *fixed_info = NULL;
     ULONG out_buf_len      = 0;
 
@@ -31,7 +42,12 @@ std::string get_dns() {
         return "";
     }
 
-    return std::string(fixed_info->DnsServerList.IpAddress.String);
+    return std::string(fixed_info->DnsServerList.IpAddress.String) + ":53";
+#else
+    res_state res = (res_state)malloc(sizeof(struct __res_state));
+    res_ninit(res);
+    return std::format("{}:{}", inet_ntoa(res->nsaddr_list[0].sin_addr), htons(res->nsaddr_list[0].sin_port));
+#endif
 }
 
 }  // namespace network
